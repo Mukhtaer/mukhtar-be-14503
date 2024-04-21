19f519e630d33b7f4078e09805a05ef43a209eed
Mukhtar Sani <mukhtarsani20@gmail.com>
2024-04-22 05:52:25 +0800

    added vercel configurations for deployment

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::get('/movies', 'App\Http\Controllers\MovieController@index');
 Route::post('/add_movie', 'App\Http\Controllers\MovieController@add_movie');
 Route::get('/genre', 'App\Http\Controllers\GenreController@index');
